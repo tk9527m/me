f4ff219c26588461abc32300f24e9a3b801262dd
tk9527 <3223634436@qq.com>
2023-01-19 15:16:51 +0000

    上传文件至 'ouge'

---
+++ ouge/3.php
@@ -0,0 +1,79 @@
+{
+  "storeHouse": [
+  {
+    "sourceName": "派大星1号仓库",
+    "sourceUrl": "https://box.nxog.top/1"
+  },
+  {
+    "sourceName": "派大星2号仓库",
+    "sourceUrl": "https://box.nxog.top/2"
+  },
+  {
+    "sourceName": "派大星3号仓库",
+    "sourceUrl": "https://box.nxog.top/3"
+  },
+  {
+    "sourceName": "派大星4号仓库",
+    "sourceUrl": "https://box.nxog.top/4"
+  },
+  {
+    "sourceName": "派大星5号仓库",
+    "sourceUrl": "https://box.nxog.top/5"
+  },
+  {
+    "sourceName": "派大星6号仓库",
+    "sourceUrl": "https://box.nxog.top/6"
+  },
+  {
+    "sourceName": "派大星7号仓库",
+    "sourceUrl": "https://box.nxog.top/7"
+  },
+  {
+    "sourceName": "派大星8号仓库",
+    "sourceUrl": "https://box.nxog.top/8"
+  },{
+    "sourceName": "派大星9号仓库",
+    "sourceUrl": "https://box.nxog.top/9"
+  },
+  {
+    "sourceName": "派大星10号仓库",
+    "sourceUrl": "https://box.nxog.top/10"
+  },
+  {
+    "sourceName": "派大星11号仓库",
+    "sourceUrl": "https://box.nxog.top/11"
+  },
+  {
+    "sourceName": "派大星12号仓库",
+    "sourceUrl": "https://box.nxog.top/12"
+  },
+  {
+    "sourceName": "派大星13号仓库",
+    "sourceUrl": "https://box.nxog.top/13"
+  },
+  {
+    "sourceName": "派大星14号仓库",
+    "sourceUrl": "https://box.nxog.top/14"
+  },
+  {
+    "sourceName": "派大星15号仓库",
+    "sourceUrl": "https://box.nxog.top/15"
+  },
+  {
+    "sourceName": "派大星16号仓库",
+    "sourceUrl": "https://box.nxog.top/16"
+  },
+  {
+    "sourceName": "派大星17号仓库",
+    "sourceUrl": "https://box.nxog.top/17"
+  },
+  {
+    "sourceName": "派大星18号仓库",
+     "sourceUrl": "https://box.nxog.top/18"
+  },
+  {
+    "sourceName": "派大星19号仓库",
+       "sourceUrl": "https://box.nxog.top/19"
+  }
+  ]
+}
---
+++ ouge/4.php
@@ -0,0 +1,9 @@
+{
+"storeHouse": [
+
+{"sourceName":"💚多多①号仓","sourceUrl":"http://tv.nxog.top/dc/多多/1.php?b=多多"},
+{"sourceName":"💚多多②号仓","sourceUrl":"http://tv.nxog.top/dc/多多/2.php?b=多多"},
+{"sourceName":"💚多多③号仓","sourceUrl":"http://tv.nxog.top/dc/多多/3.php?b=多多"},
+{"sourceName":"🧡多多④号仓","sourceUrl":"http://tv.nxog.top/dc/多多/4.php?b=多多"},
+{"sourceName":"🧡多多⑤号仓","sourceUrl":"http://tv.nxog.top/dc/多多/5.php?b=多多"}
+]}
