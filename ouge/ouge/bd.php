2bfe3a3f6f96d538c0d62f7d0d4ba3b835047d15
tk9527 <3223634436@qq.com>
2023-01-19 15:24:48 +0000

    上传文件至 'ouge/ouge'

---
+++ ouge/ouge/bd.php
@@ -0,0 +1,30 @@
+
+{
+"urls": [
+ {
+            "url": "http://tv.nxog.top/m/sapi.php?ou=%E6%AC%A7%E6%AD%8C",
+            "name": "欧歌本地包专用接口"
+        },
+  {
+            "url": "clan://localhost/tvbox/肥猫/api.json",
+            "name": "本地接口肥猫"
+        },
+          {
+            "url": "clan://localhost/tvbox/饭太硬/api.json",
+            "name": "本地js接口饭太硬"
+        },
+  
+            {
+            "url": "clan://localhost/tvbox/菜妮丝/api.json",
+            "name": "本地xBPQ接口菜妮丝"
+        }, 
+     
+         
+            {
+            "url": "clan://localhost/tvbox/唐三/api.json",
+            "name": "本地接口唐三"
+        }
+
+
+]
+}
