0f730a8f3dbc34cede1813270278a66ca3adfd87
tk9527 <3223634436@qq.com>
2023-01-19 15:17:50 +0000

    上传文件至 'ouge'

---
+++ ouge/21.php
@@ -0,0 +1,50 @@
+{
+"urls": [
+ {
+            "url": "",
+            "name": "本地接口文件夹tvbox文件名.json"
+        },
+  {
+            "url": "clan://localhost/tvbox/1.json",
+            "name": "文件夹tvbox文件名1.json"
+        },
+          {
+            "url": "clan://localhost/tvbox/2.json",
+            "name": "文件夹tvbox文件名2.json"
+        },
+         {
+            "url": "clan://localhost/tvbox/3.json",
+            "name": "文件夹tvbox文件名3.json"
+        }, 
+            {
+            "url": "clan://localhost/tvbox/4.json",
+            "name": "文件夹tvbox文件名4.json"
+        }, 
+            {
+            "url": "clan://localhost/tvbox/5.json",
+            "name": "文件夹tvbox文件名5.json"
+        }, 
+         
+            {
+            "url": "clan://localhost/tvbox/6.json",
+            "name": "文件夹tvbox文件名6.json"
+        }, 
+             {
+            "url": "clan://localhost/tvbox/7.json",
+            "name": "文件夹tvbox文件名7.json"
+        }, 
+             {
+            "url": "clan://localhost/tvbox/8.json",
+            "name": "文件夹tvbox文件名8.json"}, 
+            {
+            "url": "clan://localhost/tvbox/9.json",
+            "name": "文件夹tvbox文件名9.json"
+        }, 
+           {
+            "url": "clan://localhost/tvbox/10.json",
+            "name": "文件夹tvbox文件名10.json"
+        }
+
+
+]
+}
---
+++ ouge/22.php
@@ -0,0 +1,56 @@
+
+{
+  "storeHouse": [
+  
+  {
+    "sourceName": "文件夹tvbox文件名.json",
+    "sourceUrl": ""
+  }, 
+  
+  {
+    "sourceName": "1号本地仓库a.json",
+    "sourceUrl": "clan://localhost/tvbox/a.json"
+  }, 
+  
+  
+  {
+    "sourceName": "2号本地仓库b.json",
+    "sourceUrl": "clan://localhost/tvbox/b.json"
+  }, 
+    {
+    "sourceName": "3号本地仓库c.json",
+    "sourceUrl": "clan://localhost/tvbox/c.json"
+  }, 
+  {
+    "sourceName": "4号本地仓库d.json",
+    "sourceUrl": "clan://localhost/tvbox/d.json"
+  }, 
+  
+  {
+    "sourceName": "5号本地仓库e.json",
+    "sourceUrl": "clan://localhost/tvbox/e.json"
+  }
+  
+  
+  
+  
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
