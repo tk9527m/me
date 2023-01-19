6ed461a59fdc0c75e3fb48413e718967ea2979e2
tk9527 <3223634436@qq.com>
2023-01-19 15:47:46 +0000

    上传文件至 'ouge/ksdc7h'

---
+++ ouge/ksdc7h/api(1).php
@@ -0,0 +1,9 @@
+{
+    "urls": [
+{ "url": "http://tv.nxog.top/m/1.php?ou=%E6%AC%A7%E6%AD%8C&xl=4","name": "🇨🇳欧歌🇨🇳 01-19  整理" },
+ {         "url": "",         "name": "本地接口文件夹tvbox文件名.json"     },
+  {         "url": "clan://localhost/tvbox/1.json",         "name": "文件夹tvbox文件名1.json"     },       {         "url": "clan://localhost/tvbox/2.json",         "name": "文件夹tvbox文件名2.json"     },      {         "url": "clan://localhost/tvbox/3.json",         "name": "文件夹tvbox文件名3.json"     },          {         "url": "clan://localhost/tvbox/4.json",         "name": "文件夹tvbox文件名4.json"     },          {         "url": "clan://localhost/tvbox/5.json",         "name": "文件夹tvbox文件名5.json"     },                {         "url": "clan://localhost/tvbox/6.json",         "name": "文件夹tvbox文件名6.json"     },           {         "url": "clan://localhost/tvbox/7.json",         "name": "文件夹tvbox文件名7.json"     },           {         "url": "clan://localhost/tvbox/8.json",         "name": "文件夹tvbox文件名8.json"},          {         "url": "clan://localhost/tvbox/9.json",         "name": "文件夹tvbox文件名9.json"     },         {         "url": "clan://localhost/tvbox/10.json",         "name": "文件夹tvbox文件名10.json"     }
+
+
+]
+}
